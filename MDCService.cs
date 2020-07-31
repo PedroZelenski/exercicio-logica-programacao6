3c599ef9816b3e8f601367fccec7eb1dab2278fa
Zelenski <zelenski.contato@gmail.com>
2020-07-31 14:32:33 -0300

    first commit

---
+++ MDCService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_logica_programacao6
+{
+    class MDCService
+    {
+        public void CalcularMDC()
+        {
+            int num1, dividendo;
+            int num2, divisor, resto;
+
+            Console.WriteLine("Digite o primeiro número");
+            num1 = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            num2 = Convert.ToInt32(System.Console.ReadLine());
+
+            if (num1 == 0 || num2 == 0)
+                System.Console.WriteLine("Não são permitidos valores iguais a 0!");
+
+            if (num1 > num2)
+            {
+                dividendo = num1;
+                divisor = num2;
+            }
+            else
+            {
+                dividendo = num2;
+                divisor = num1;
+            }
+
+            while (dividendo % divisor != 0)
+            {
+                resto = dividendo % divisor;
+                dividendo = divisor;
+                divisor = resto;
+            }
+
+            Console.WriteLine($"MDC = {divisor}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercicio_logica_programacao6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MDCService mDCService = new MDCService();
+            mDCService.CalcularMDC();
+        }
+    }
+}
